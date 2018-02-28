c1216a97f64915837e8618fcc933c1f840cbf003
Dani Cupples <danielle.volker@gmail.com>
2018-02-27 21:05:49 -0500

    updating

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Open Budget Charlotte</h1>
+        </header>
+        <p>Our goal is to show the open budget data for Charlotte, North Carolina.</p>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
