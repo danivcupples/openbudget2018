a9a63786381e09f9d7e2ee99c614658f8fac1faf
Dani Cupples <danielle.volker@gmail.com>
2018-02-27 21:07:30 -0500

    setting up development

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to Open Budget Charlotte</h1>
         </header>
         <p>Our goal is to show the open budget data for Charlotte, North Carolina.</p>
+        <p>Look here for data visualizations later.</p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
